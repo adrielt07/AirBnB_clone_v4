5c474147f7a4d271c9ab85dfd2f229fa890f189f
adrielt07 <adrieltolentino@outlook.com>
2018-09-10 00:47:49 +0000

    implement search functionality

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,4 @@
-// This script will listen if box check to account for amenity id or remove
+// This script will listen if box check to account for amenity id or removez
 let list = {}
 let keys = []
 $( document ).ready(function() {
@@ -25,43 +25,37 @@ $( document ).ready(function() {
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    data: '{}',
+    data: JSON.stringify({}),
     success: function(data) { myfunc(data) },
     contentType: "application/json",
     dataType: 'json'
   });
 
-  function myfunc(places)
-  {
-    $(places).each(function (index, place) {
-      $.getJSON('http://0.0.0.0:5001/api/v1/users/' + place.user_id, function (data, stats) {
-	$('SECTION.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests' + '</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="user"><strong>Owner:' + data.first_name + '</strong></div><div class="description">' + place.description + '</div></article>');
-	});
-    });
-  }
-
-/*
-// When button clicked, post request w/ list of amenities checked
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search',
-    data: '{amenities: []}',
-    success: function(data) {myfunc(data) },
-    contentType: "application/json",
-    dataType: "json";
-  });
-
-*/
+// Sends a Post Request when Search button is clicked
   $('BUTTON').on('click', function () {
     if ($(this).data('clicked', true)) {
       $.ajax({
 	type: 'POST',
 	url: 'http://0.0.0.0:5001/api/v1/places_search',
-	data: '{}',
-	success: function(data) {myfunc(data) },
+	data: JSON.stringify({"amenities": Object.values(list)}),
+	success: function(data) { myfunc(data) },
 	contentType: "application/json",
-	dataType: "json";
+	dataType: "json"
       });
     };
   });
+
+  function myfunc(places) {
+    alert("Sending Post request")
+    $(places).each(function (index, place) {
+      $.getJSON('http://0.0.0.0:5001/api/v1/users/' + place.user_id, function (data, stats) {
+	$('SECTION.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + '$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest"><i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests' + '</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div><div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + ' Bathroom</div></div><div class="user"><strong>Owner:' + data.first_name + data.last_name + '</strong></div><div class="description">' + place.description + '</div></article>');
+      });
+    });
+    updatePlaces(places);
+  }
+
+  function updatePlaces(data) {
+    $( "SECTION.places" ).html(data);
+  }
 });
